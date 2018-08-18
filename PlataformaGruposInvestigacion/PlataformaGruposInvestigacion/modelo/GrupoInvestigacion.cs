76e226878364a7630c8400537b7b7dcca748e7bf
Daniela <daniela08699@gmail.com>
2018-08-18 13:21:51 -0500

    grupo investigacion

--- PlataformaGruposInvestigacion/PlataformaGruposInvestigacion/modelo/GrupoInvestigacion.cs
+++ PlataformaGruposInvestigacion/PlataformaGruposInvestigacion/modelo/GrupoInvestigacion.cs
@@ -3,8 +3,95 @@ namespace PlataformaGruposInvestigacion.modelo
 {
     public class GrupoInvestigacion
     {
-        public GrupoInvestigacion()
+     //-----------------------------------------------------------------------------------------------------------------
+     //Atributos
+     //-----------------------------------------------------------------------------------------------------------------
+        private String nombre;
+        private int codigo;
+        private String clasificacion;
+        private String ciudad;
+        private String areaInvestigacion;
+        private String region;
+
+      //-----------------------------------------------------------------------------------------------------------------
+      //Constructor
+      //-----------------------------------------------------------------------------------------------------------------
+        public GrupoInvestigacion(String nombre, int codigo,String clasificacion, String ciudad, String areaInvestigacion, String region){
+            this.nombre = nombre;
+            this.codigo = codigo; 
+            this.clasificacion = clasificacion;
+            this.ciudad = ciudad;
+            this.areaInvestigacion = areaInvestigacion;
+            this.region = region;
+        }
+      //-----------------------------------------------------------------------------------------------------------------
+      //Metodos
+      //-----------------------------------------------------------------------------------------------------------------
+        public String Nombre
         {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+            set
+            {
+                codigo = value;
+            }
+        }
+        public String Clasificacion
+        {
+            get
+            {
+                return clasificacion;
+            }
+            set
+            {
+                clasificacion = value;
+            }
+        }
+        public String Ciudad
+        {
+            get
+            {
+                return ciudad;
+            }
+            set
+            {
+                ciudad = value;
+            }
+        }
+        public String AreaInvestigacion
+        {
+            get
+            {
+                return areaInvestigacion;
+            }
+            set
+            {
+                areaInvestigacion = value;
+            }
+        }
+        public String Region
+        {
+            get
+            {
+                return region;
+            }
+            set
+            {
+                region = value;
+            }
         }
     }
 }
