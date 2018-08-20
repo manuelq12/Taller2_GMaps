12bb59c85caef2b468a60fbbc7d0c3bafd42441b
manuelq12 <manuelq12@users.noreply.github.com>
2018-08-20 18:19:13 -0500

    Ojala sirva

--- PlataformaGruposInvestigacion/PlataformaGruposInvestigacion/interfaz/interfazPrincipal.Designer.cs
+++ PlataformaGruposInvestigacion/PlataformaGruposInvestigacion/interfaz/interfazPrincipal.Designer.cs
@@ -33,6 +33,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.butReportes = new System.Windows.Forms.Button();
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,20 +42,20 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(477, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(179, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1163, 89);
+            this.label1.Size = new System.Drawing.Size(464, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "Grupos Investigación Colombia";
             // 
             // butServicios
             // 
             this.butServicios.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butServicios.Location = new System.Drawing.Point(1683, 854);
-            this.butServicios.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.butServicios.Location = new System.Drawing.Point(631, 347);
+            this.butServicios.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.butServicios.Name = "butServicios";
-            this.butServicios.Size = new System.Drawing.Size(405, 91);
+            this.butServicios.Size = new System.Drawing.Size(152, 38);
             this.butServicios.TabIndex = 2;
             this.butServicios.Text = "Servicios";
             this.butServicios.UseVisualStyleBackColor = true;
@@ -65,20 +66,20 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::PlataformaGruposInvestigacion.Properties.Resources.google2;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(1707, 379);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.pictureBox1.Location = new System.Drawing.Point(640, 159);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(357, 329);
+            this.pictureBox1.Size = new System.Drawing.Size(134, 138);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // butReportes
             // 
             this.butReportes.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butReportes.Location = new System.Drawing.Point(1682, 1088);
-            this.butReportes.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.butReportes.Location = new System.Drawing.Point(631, 456);
+            this.butReportes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.butReportes.Name = "butReportes";
-            this.butReportes.Size = new System.Drawing.Size(405, 107);
+            this.butReportes.Size = new System.Drawing.Size(152, 45);
             this.butReportes.TabIndex = 6;
             this.butReportes.Text = "Reportes";
             this.butReportes.UseVisualStyleBackColor = true;
@@ -92,12 +93,10 @@
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(32, 148);
-            this.gMapControl1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.gMapControl1.Location = new System.Drawing.Point(12, 62);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 18;
             this.gMapControl1.MinZoom = 2;
-            this.gMapControl1.MouseWheelZoomEnabled = true;
             this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
             this.gMapControl1.Name = "gMapControl1";
             this.gMapControl1.NegativeMode = false;
@@ -107,24 +106,36 @@
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(1637, 1226);
+            this.gMapControl1.Size = new System.Drawing.Size(614, 514);
             this.gMapControl1.TabIndex = 7;
             this.gMapControl1.Zoom = 5D;
             this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(631, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 59);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Buscar Mapa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // interfazPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PlataformaGruposInvestigacion.Properties.Resources.fondo1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(2115, 1414);
+            this.ClientSize = new System.Drawing.Size(811, 530);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gMapControl1);
             this.Controls.Add(this.butReportes);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.butServicios);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "interfazPrincipal";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.interfazPrincipal_Load);
@@ -141,6 +152,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button butReportes;
         private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
