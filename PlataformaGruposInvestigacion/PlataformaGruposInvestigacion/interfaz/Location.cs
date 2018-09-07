0b35aea8638096537fb535400425f8687ce3cbbe
daniela8699 <daniela08699@gmail.com>
2018-09-07 10:17:26 -0500

    cambios

---
+++ PlataformaGruposInvestigacion/PlataformaGruposInvestigacion/interfaz/Location.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlataformaGruposInvestigacion.interfaz
+{
+    class Location
+    {
+        public List<Municipality> MunicipalityList { get; set; }
+
+
+        public Location()
+        {
+            MunicipalityList = generarLocaciones();
+        }
+
+        public static List<Municipality> generarLocaciones()
+        {
+            List<Municipality> temporalList = new List<Municipality>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(@"C:\Users\Sara\Source\Repos\GMaps-.NET\WindowsFormsApp1\WindowsFormsApp1\Datos\ubicacionesMunicipios.txt");
+
+                String line;
+                int c = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    String[] atributos = line.Split(';');
+
+                    String nombre = atributos[0];
+                    String cooY1 = atributos[1];
+                    String cooX1 = atributos[2];
+                    String cooY2 = atributos[3];
+                    String cooX2 = atributos[4];
+
+                    Municipality mun = new Municipality(nombre, cooY1, cooX1, cooY2, cooX2);
+                    //Add element to the municipalityList
+                    temporalList.Add(mun);
+
+                    c++;
+                }
+
+                //MessageBox.Show(temporalList.Count()+"");
+
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            return temporalList;
+        }
+    }
+}
